b4bea46cf85b67160990c6e3705d7bc918d1d746
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-08-29 19:53:08 +0700

    .

--- server.js
+++ server.js
@@ -4,31 +4,29 @@ const qrcode = require('qrcode');
 
 const app = express();
 app.use(express.json());
+
 let qrCodeData = '';
 let isConnected = false;
 
 const client = new Client();
-
 client.on('qr', (qr) => {
-    // Generate QR code and store it
     qrcode.toDataURL(qr, (err, url) => {
         if (err) {
             console.error('Failed to generate QR code:', err);
         } else {
-            qrCodeData = url; // Store QR code as data URL
-            isConnected = false; // Set connected to false if QR is regenerated
+            qrCodeData = url;
+            isConnected = false;
         }
     });
 });
 
 client.on('ready', () => {
     console.log('WhatsApp Client is ready!');
-    isConnected = true; // Mark as connected when ready
+    isConnected = true;
 });
 
 client.initialize();
 
-// Serve QR Code to the client
 app.get('/qr', (req, res) => {
     if (qrCodeData && !isConnected) {
         res.json({ qrCode: qrCodeData });
@@ -39,13 +37,8 @@ app.get('/qr', (req, res) => {
     }
 });
 
-// Endpoint to check if device is connected
 app.get('/status', (req, res) => {
-    if (isConnected) {
-        res.json({ connected: true });
-    } else {
-        res.json({ connected: false });
-    }
+    res.json({ connected: isConnected });
 });
 
 app.post('/send-message', async (req, res) => {
@@ -56,30 +49,17 @@ app.post('/send-message', async (req, res) => {
     }
 
     try {
-        // Mengumpulkan semua promise untuk pengiriman pesan
         const sendPromises = numbers.map(number => {
             const formattedNumber = `${number}@c.us`;
             return client.sendMessage(formattedNumber, message);
         });
 
-        // Menunggu semua promise selesai
         await Promise.all(sendPromises);
 
-        // Mengirimkan satu respons setelah semua pesan dikirim
         res.json({ status: 'Messages sent successfully' });
     } catch (error) {
-        // Menangani kesalahan jika terjadi
-        if (!res.headersSent) { // Pastikan headers belum dikirim
-            res.status(500).json({ error: 'Failed to send messages', details: error });
-        }
+        res.status(500).json({ error: 'Failed to send messages', details: error });
     }
 });
 
-
-// Serve static files (frontend)
-app.use(express.static('public'));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app; // Export Express app as a handler
