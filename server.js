1f06a764e5bf34e5e9010c06fc9dcbb619cc6179
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-08-30 10:34:00 +0700

    .

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 let qrCodeData = '';
 let isConnected = false;
@@ -105,7 +108,7 @@ app.post('/send-message', async (req, res) => {
 // Serve static files (frontend)
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
